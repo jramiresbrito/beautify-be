f65e36c815673ad770e1f5c103f044fe8be92bb2
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-01-26 00:26:01 -0300

    Feature: Models Loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ ok: false }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'User 1',
+    email: 'user.1@email.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
