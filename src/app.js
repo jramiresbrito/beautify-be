d19ad855e7dfd9eab015c30bfc79dc1336219fca
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-01-25 23:10:35 -0300

    Chore: Set hot reload dev server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('dotenv/config');
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -18,4 +18,4 @@ class App {
   }
 }
 
-module.exports = new App().server;
+export default new App().server;
