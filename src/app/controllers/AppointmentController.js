2c5728b51845a0b5d94cadf85d36f93b46bf85cb
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-01-29 16:26:29 -0300

    Feature: Extract Cancellation Mail Job from Appointment Controller

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -127,19 +127,6 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt_br,
-        }),
-      },
-    });
-
     const { user_id, provider_id, date, canceled_at } = appointment;
 
     return res.json({ id, user_id, provider_id, date, canceled_at });
